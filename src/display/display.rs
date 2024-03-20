61251c3843867cd6e4860aa02ac33292d24d265f
david.rimmer <david.rimmer@awe.co.uk>
2024-03-20 17:08:31 +0000

    make terminal pixels square

--- src/display/display.rs
+++ src/display/display.rs
@@ -24,13 +24,19 @@ impl Display for CrossTermDisplay {
     fn draw_display(&mut self, display_data: &[[bool; 32]; 64]) -> Result<(), io::Error> {
         for y in 0..32 {
             for x in 0..64 {
+                let x0 = x * 2;
+                let x1 = x0 + 1;
                 if display_data[x][y] {
                     self.stdout
-                        .queue(cursor::MoveTo(x.try_into().unwrap(), y.try_into().unwrap()))?
+                        .queue(cursor::MoveTo(x0.try_into().unwrap(), y.try_into().unwrap()))?
+                        .queue(style::PrintStyledContent("█".white()))?
+                        .queue(cursor::MoveTo(x1.try_into().unwrap(), y.try_into().unwrap()))?
                         .queue(style::PrintStyledContent("█".white()))?;
                 } else {
                     self.stdout
-                        .queue(cursor::MoveTo(x.try_into().unwrap(), y.try_into().unwrap()))?
+                        .queue(cursor::MoveTo(x0.try_into().unwrap(), y.try_into().unwrap()))?
+                        .queue(style::PrintStyledContent("█".hidden()))?
+                        .queue(cursor::MoveTo(x1.try_into().unwrap(), y.try_into().unwrap()))?
                         .queue(style::PrintStyledContent("█".hidden()))?;
                 }
             }
